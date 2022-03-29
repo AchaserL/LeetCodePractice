932794bcf223ceaa9c6277b8694ad46e9739a768
GuiyangLiu <47599530+AchaserL@users.noreply.github.com>
2022-03-29 15:03:52 +0800

    Update nearestPalindromic.py

--- nearestPalindromic.py
+++ nearestPalindromic.py
@@ -5,13 +5,13 @@ class Solution:
         selfPrefix = int(n[:(m+1)//2]) # 取n的前一半：12345取123
         for x in range(selfPrefix - 1, selfPrefix + 2): # 在[selfPrefix-1, selfPrefix+1]中遍历可行解
             y = x if m % 2 == 0 else x // 10
-            print('y', y)
+#             print('y', y)
             while y:
                 x = x * 10 + y % 10
                 y //= 10
             candidates.append(x)
 
-        print(candidates)
+#         print(candidates)
         ans = -1
         selfNumber = int(n)
         for candidate in candidates:
